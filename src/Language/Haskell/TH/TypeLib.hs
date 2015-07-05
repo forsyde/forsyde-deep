forsyde/forsyde-deep

commit 727bb136246069bc469fb987ee18d21f6d340fb4

Use new mkTyCon3 function instead of obsolete mkTyCon

src/Language/Haskell/TH/TypeLib.hs
@@ -32,6 +32,7 @@ module Language.Haskell.TH.TypeLib
   type2TypeRep)
  where
 
+import Data.Typeable.Internal
 import Data.Dynamic
 import Language.Haskell.TH (Type(..), Cxt, TyVarBndr(..), pprint, mkName)
 import Text.Regex.Posix ((=~))
@@ -309,7 +310,10 @@ tyConStr2Type str  = ConT $ mkName str
 -- Get the type constructor corresponding to a String
 -- in form of a type representation
 strCon :: String -> TypeRep
-strCon str = mkTyCon str `mkTyConApp` []
+strCon str = mkTyCon3 pkg mod name `mkTyConApp` []
+        where
+        (_, _, _, [pkg,mod,name]) = (str =~ "^(.+)\\.(.+)\\.(.+)$" ::(String, String, String, [String]))
+
 
 -- Get the type constructor corresponding to a typeable value
 -- in form of a type representation

commit 66b44213c80a08586318289785c9343025c120ee

Correct wrong parsing of names in TypeLib
The previous commit 727bb13 introduced wrong string parsing for the
name. Use TH API instead for name parsing

src/Language/Haskell/TH/TypeLib.hs
@@ -34,9 +34,9 @@ module Language.Haskell.TH.TypeLib
 
 import Data.Typeable.Internal
 import Data.Dynamic
-import Language.Haskell.TH (Type(..), Cxt, TyVarBndr(..), pprint, mkName)
+import Language.Haskell.TH (Type(..), Cxt, TyVarBndr(..), pprint, mkName, nameModule, nameBase)
 import Text.Regex.Posix ((=~))
-import Data.Maybe(isJust)
+import Data.Maybe(isJust, fromMaybe)
 
 -- Due to type translations
 import GHC.Exts (RealWorld)
@@ -310,9 +310,12 @@ tyConStr2Type str  = ConT $ mkName str
 -- Get the type constructor corresponding to a String
 -- in form of a type representation
 strCon :: String -> TypeRep
-strCon str = mkTyCon3 pkg mod name `mkTyConApp` []
+strCon str = mkTyCon3 pkg mod base `mkTyConApp` []
         where
-        (_, _, _, [pkg,mod,name]) = (str =~ "^(.+)\\.(.+)\\.(.+)$" ::(String, String, String, [String]))
+                name = mkName str
+                pkg  = ""
+                mod  = fromMaybe "" (nameModule name)
+                base = nameBase name
 
 
 -- Get the type constructor corresponding to a typeable value