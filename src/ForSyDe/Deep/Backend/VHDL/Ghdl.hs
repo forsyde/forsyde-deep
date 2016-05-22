00e5ba84ba921dd3b808be8c48e6dbcdb90ca777
Hendrik Woidt <woidt@kth.se>
2016-05-22 10:50:46 +0000

    Clean up and refactor GHDL tool driver

--- src/ForSyDe/Deep/Backend/VHDL/Ghdl.hs
+++ src/ForSyDe/Deep/Backend/VHDL/Ghdl.hs
@@ -22,6 +22,7 @@ import ForSyDe.Deep.ForSyDeErr
 import ForSyDe.Deep.Config (getDataDir)
 
 import Data.List (intersperse)
+import Data.Maybe (isJust)
 import Control.Monad (liftM, when)
 import Control.Monad.State (gets)
 import System.Directory (findExecutable,
@@ -34,6 +35,56 @@ import System.IO
 import System.FilePath ((</>))
 import qualified Language.Haskell.TH as TH (Exp)
 
+data GhdlCommand = Analyze | Elaborate | Compile | Import | Run deriving Eq
+instance Show GhdlCommand where
+    show Analyze   = "-a"
+    show Elaborate = "-e"
+    show Compile   = "-c"
+    show Import    = "-i"
+    show Run       = "-r"
+
+type Path = String
+
+data GhdlEnv =  GhdlEnv { sysId           :: String
+                        , sysTb           :: String
+                        , syslib          :: String
+                        , tbFile          :: Path
+                        , tbExecutable    :: Path
+                        , libFile         :: Path
+                        , workFiles       :: [Path]
+                        , forsydeLibFile  :: Path
+                        , forsydeLibDir   :: Path
+                        , systemLibDir    :: Path
+                        , workDir         :: Path
+                        , paths           :: [Path]
+                        }
+
+mkGhdlEnv sys osDataPath = 
+    GhdlEnv
+      { sysId     = sysId
+      , sysTb     = sysTb
+      , syslib    = syslib
+      , libFile   = syslib </> (syslib ++ ".vhd")
+      , tbFile    = "test" </> (sysTb ++ ".vhd")
+      , forsydeLibFile = osDataPath</>"lib"</>"forsyde.vhd"
+      , workFiles = ("work" </> (sysId ++ ".vhd")) :
+             map (("work"</>).(++".vhd").sid.readURef.unPrimSysDef)
+                 (subSys sys)
+      , forsydeLibDir = forsydeLibDir
+      , systemLibDir  = systemLibDir
+      , workDir       = workDir
+      , tbExecutable  = workDir  </> sysTb
+      , paths = [forsydeLibDir, systemLibDir, workDir]
+    }
+    where
+        sysId           = sid sys
+        syslib          = sysId ++ "_lib"
+        sysTb           = sysId ++ "_tb"
+        workDir         = "work"    </> "ghdl"
+        forsydeLibDir   = "forsyde" </> "ghdl"
+        systemLibDir    = syslib    </> "ghdl"
+
+
 -- | Generate a testbench and execute it with GHDL
 --   (Note: the initial and final CWD will be / )
 executeTestBenchGhdl :: Maybe Int -- ^ Number of cycles to simulate          
@@ -44,87 +95,64 @@ executeTestBenchGhdl :: Maybe Int -- ^ Number of cycles to simulate
                                              --   is expressed as a string
 executeTestBenchGhdl mCycles stimuli = do
  -- Check if GHDL is installed
- installed <- liftIO $ isGhdlInstalled
- when (not installed) (throwFError GhdlFailed) 
+ installed <- liftIO isGhdlInstalled
+ unless installed (throwFError GhdlFailed) 
  
  -- compile testbench
  cycles <- writeVHDLTestBench mCycles stimuli
- sysid <- gets (sid.globalSysDef.global)
- -- change to sysid/vhdl/
- liftIO $ setCurrentDirectory (sysid </> "vhdl")
 
- -- get the name of the vhdl files to compile
- sys <- gets (globalSysDef.global)
- dataPath <- liftIO $ getDataDir
- let sysId     = sid sys
-     sysTb     = sysId ++ "_tb"
-     syslib    = sysId ++ "_lib"
-     libFile   = syslib </> (syslib ++ ".vhd")
-     tbFile    = "test" </> (sysTb ++ ".vhd")
-     workFiles = ("work" </> (sysId ++ ".vhd")) :
-          map (("work"</>).(++".vhd").sid.readURef.unPrimSysDef)
-              (subSys sys)
-     forsydeLibDir = "forsyde"</>"ghdl"
-     systemLibDir  =  syslib  </>"ghdl"
-     workDir       = "work"   </>"ghdl"
-     tbExecutable  = workDir  </>sysTb
-     paths = [forsydeLibDir, systemLibDir, workDir]
- tmpdir <- liftIO getTemporaryDirectory 
- (file, handle) <- liftIO $ openTempFile tmpdir "tb_out.txt"
- -- we close the temporal file to avoid opening problems with vsim on windows
- liftIO $ hClose handle 
+ -- assemble all the file paths for compilation
+ sysid    <- gets (sid.globalSysDef.global)
+ sys      <- gets (globalSysDef.global)
+ dataPath <- liftIO getDataDir
+ let env = mkGhdlEnv sys dataPath
 
- _ <- liftIO $ mapM (createDirectoryIfMissing True) paths
+ -- set up directory structure and tmp files
+ file <- liftIO $ do
+        setCurrentDirectory (sysid </> "vhdl")
+        tmpdir         <- getTemporaryDirectory 
+        (file, handle) <- openTempFile tmpdir "tb_out.txt"
+        hClose handle -- close handle to avoid opening problems in windows
+        mapM_ (createDirectoryIfMissing True) $ paths env
+        return file
 
- runGhdlCommand Analyze "forsyde" forsydeLibDir [] [dataPath</>"lib"</>"forsyde.vhd"] []
- runGhdlCommand Analyze syslib systemLibDir [forsydeLibDir] [libFile] []
- runGhdlCompile sysTb workDir [forsydeLibDir, systemLibDir] $ tbFile:workFiles
+ -- analyze the installed forsyde library
+ runGhdlCommand Analyze "forsyde" 
+                        (forsydeLibDir env) 
+                        [] 
+                        [forsydeLibFile env] 
+                        []
+
+ -- analyze the generated system library
+ runGhdlCommand Analyze (syslib env) 
+                        (systemLibDir env) 
+                        [forsydeLibDir env] 
+                        [libFile env] 
+                        []
+
+ -- compile test bench hierarchy
+ runGhdlCompile (sysTb env)
+                (workDir env) 
+                [forsydeLibDir env, systemLibDir env] 
+                tbFile env:workFiles env
+
  -- TODO: run test bench with "tb_out.txt" as output file
- runGhdlCommand Run sysTb workDir [forsydeLibDir, systemLibDir] [] ["--stop-time="++(show $ cycles*10)++"ns"]
+ runGhdlCommand Run (sysTb env) 
+                    (workDir env) 
+                    [forsydeLibDir, systemLibDir] 
+                    [] 
+                    ["--stop-time="++show (cycles*10)++"ns"]
 
- handle2 <- liftIO $ openFile file ReadMode
- flatOut <- liftIO $ hGetContents handle2
+ -- read test output data
+ testOutput <- liftIO $ do 
+         handle <- openFile file ReadMode
+         output <- hGetContents handle
+         -- go back to the original directory
+         setCurrentDirectory (".." </> "..")
+         return output
 
- -- go back to the original directory
- liftIO $ setCurrentDirectory (".." </> "..")
- parseTestBenchOut flatOut
+ parseTestBenchOut testOutput
 
--- | Compile the generated VHDL code with GHDL
---   (Note: the initial and final CWD will be /systemName/vhdl )
-compileResultsGhdl :: VHDLM ()
-compileResultsGhdl = do
- -- Check if Ghdl is installed
- installed <- liftIO $ isGhdlInstalled
- when (not installed) (throwFError GhdlFailed) 
- -- get the name of the vhdl files to compile
- sys <- gets (globalSysDef.global)
- dataPath <- liftIO $ getDataDir
- let sysId = sid sys
-     syslib = sysId ++ "_lib"
-     libFile = syslib </> (syslib ++ ".vhd")
-     workFiles = ("work" </> (sysId ++ ".vhd")) :
-          map (("work"</>).(++".vhd").sid.readURef.unPrimSysDef)
-              (subSys sys)
-     forsydeLibDir = "forsyde"</>"ghdl"
-     systemLibDir  =  syslib  </>"ghdl"
-     workDir       = "work"   </>"ghdl"
-     paths = [forsydeLibDir, systemLibDir, workDir]
-
- _ <- liftIO $ mapM (createDirectoryIfMissing True) paths
-
- runGhdlCommand Analyze "forsyde" forsydeLibDir [] [dataPath</>"forsyde.vhd"] []
- runGhdlCommand Analyze syslib systemLibDir [forsydeLibDir] [libFile] []
- runGhdlCommand Analyze "work" workDir [forsydeLibDir, systemLibDir] workFiles []
-
-data GhdlCommand = Analyze | Elaborate | Compile | Import | Run deriving Eq
-instance Show GhdlCommand where
-    show Analyze   = "-a"
-    show Elaborate = "-e"
-    show Compile   = "-c"
-    show Import    = "-i"
-    show Run       = "-r"
-
-type Path = String
 
 runGhdlCompile :: String -> Path -> [Path] -> [Path] -> VHDLM ()
 runGhdlCompile toplevel workdir libPaths files = 
@@ -137,7 +165,7 @@ runGhdlCommand :: GhdlCommand
                 -> String -> Path -> [Path] -> [Path] 
                 -> [String] 
                 -> VHDLM ()
-runGhdlCommand command libName workdir libPaths files extraOpts = do
+runGhdlCommand command libName workdir libPaths files extraOpts =
   runCommand "ghdl" $ cmd ++ paths ++ opts ++ files ++ extraOpts
     where
       cmd   = [show command]
@@ -151,26 +179,17 @@ runCommand :: String -- ^ Command to execute
               -> [String] -- ^ Command arguments
               -> VHDLM ()
 runCommand command args = do
-  success <- liftIO $ runWait msg command args
-  when (not success) (throwFError GhdlFailed)
- where msg = "Running: " ++ command ++ " " ++ (concat $ intersperse " " args)
-
-
--- | Run a process, previously announcing a message and waiting for it
---   to finnish its execution.
-runWait :: String -- ^ message to show
-        -> FilePath -- ^ command to execute 
-        -> [String] -- ^ command arguments
-        -> IO Bool -- ^ Did the execution end succesfully?
-runWait msg proc args = do
-           putStrLn msg 
-           h <- runProcess proc args Nothing Nothing Nothing Nothing Nothing
-           code <- waitForProcess h
-           return $ code == ExitSuccess 
+  success <- liftIO $ do
+      putStrLn msg 
+      h <- runProcess command args Nothing Nothing Nothing Nothing Nothing
+      code <- waitForProcess h
+      return $ code == ExitSuccess 
+  unless success (throwFError GhdlFailed)
+ where msg = "Running: " ++ command ++ " " ++ unwords args
 
 
 -- Look for GHDL executables
 isGhdlInstalled :: IO Bool
 isGhdlInstalled =  executablePresent "ghdl"
- where executablePresent = (liftM (maybe False (\_-> True))) .findExecutable
+ where executablePresent = liftM isJust .findExecutable
 
